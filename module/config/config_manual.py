9f296de605c6d24649cab25c9d5e9cc36ba96c17
juzwcz233 <145629366+juzwcz233@users.noreply.github.com>
2024-05-28 18:23:02 +0800

    Update config_manual.py

--- module/config/config_manual.py
+++ module/config/config_manual.py
@@ -320,7 +320,7 @@ class ManualConfig:
     """
     OS_ACTION_POINT_BOX_USE = True
     OS_ACTION_POINT_PRESERVE = 0
-    OS_CL1_YELLOW_COINS_PRESERVE = 100000
+    OS_CL1_YELLOW_COINS_PRESERVE = 40000
 
     """
     module.os.globe_detection
